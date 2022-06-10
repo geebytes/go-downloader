80792982f384d0380b95aa3c2cee571b70011d89
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2022-06-10 14:11:09 +0800

    chroe:adjsut dirs struct

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/geebytes/go-downloader/pb"
 	grpc "google.golang.org/grpc"
 )
 
@@ -13,25 +14,25 @@ var downloader *Downloader = NewDownloader("/data/work/download")
 type ServerInterface interface {
 	Callback(src string, err error) (string, error)
 	// Download(ctx context.Context, in *DownloadRequest) (*DownloadResponse, error)
-	DownloaderServer
+	pb.DownloaderServer
 }
 
 type DefaultServer struct {
-	UnimplementedDownloaderServer
+	pb.UnimplementedDownloaderServer
 }
 
 func (s *DefaultServer) Callback(src string, err error) (string, error) {
 	return src, err
 }
-func (s *DefaultServer) Download(ctx context.Context, in *DownloadRequest) (*DownloadResponse, error) {
+func (s *DefaultServer) Download(ctx context.Context, in *pb.DownloadRequest) (*pb.DownloadResponse, error) {
 	dst, err := downloader.Download(in.Request, in.FileName)
 	dst, err = s.Callback(dst, err)
 	if err != nil {
 		fmt.Println(err.Error())
-		return &DownloadResponse{Dst: dst, Status: 500, Msg: err.Error()}, err
+		return &pb.DownloadResponse{Dst: dst, Status: 500, Msg: err.Error()}, err
 
 	}
-	return &DownloadResponse{Dst: dst, Status: 200}, nil
+	return &pb.DownloadResponse{Dst: dst, Status: 200}, nil
 
 }
 
@@ -42,7 +43,7 @@ func StartServer(server ServerInterface) {
 		return
 	}
 	s := grpc.NewServer()               // 创建gRPC服务器
-	RegisterDownloaderServer(s, server) // 在gRPC服务端注册服务
+	pb.RegisterDownloaderServer(s, server) // 在gRPC服务端注册服务
 	err = s.Serve(lis)
 	if err != nil {
 		panic(err)
