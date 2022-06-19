980b6d6970b3c23d619a671fad8ef9da2ba8e2ce
vforfreedom96@gmail.com <vforfreedom96@gmail.com>
2022-06-19 11:25:33 +0800

    feat:update common

---
+++ Makefile
@@ -0,0 +1,22 @@
+GOPATH:=$(shell go env GOPATH)
+.PHONY: init
+init:
+    go install google.golang.org/grpc
+
+	go get -u github.com/golang/protobuf/proto
+	go get -u google.golang.org/grpc/cmd/protoc-gen-go-grpc
+.PHONY: proto
+proto:
+	protoc --go_out=. --go_opt=paths=source_relative  pb/downloader.proto
+	
+# .PHONY: build
+# build:
+# 	go build -o registerConfiguration *.go
+
+# .PHONY: test
+# test:
+# 	go test -v ./... -cover
+
+# .PHONY: docker
+# docker:
+# 	docker build . -t registerConfiguration:latest
--- server.go
+++ server.go
@@ -3,13 +3,10 @@ package godownloader
 import (
 	context "context"
 	"fmt"
-	"log"
-	"time"
+	"net"
 
-	"github.com/asim/go-micro/plugins/server/grpc/v4"
 	"github.com/geebytes/go-downloader/pb"
-	"go-micro.dev/v4"
-	"go-micro.dev/v4/registry"
+	grpc "google.golang.org/grpc"
 )
 
 var downloader *Downloader = NewDownloader("/data/work/download")
@@ -17,54 +14,38 @@ var downloader *Downloader = NewDownloader("/data/work/download")
 type ServerInterface interface {
 	Callback(src string, err error) (string, error)
 	// Download(ctx context.Context, in *DownloadRequest) (*DownloadResponse, error)
-	pb.DownloaderHandler
+	pb.DownloaderServer
 }
 
 type DefaultServer struct {
-	// pb.UnimplementedDownloaderServer
+	pb.UnimplementedDownloaderServer
 }
 
 func (s *DefaultServer) Callback(src string, err error) (string, error) {
 	return src, err
 }
-func (s *DefaultServer) Download(ctx context.Context, in *pb.DownloadRequest, out *pb.DownloadResponse) error {
+func (s *DefaultServer) Download(ctx context.Context, in *pb.DownloadRequest) (*pb.DownloadResponse, error) {
 	dst, err := downloader.Download(in.Request, in.FileName)
 	dst, err = s.Callback(dst, err)
 	if err != nil {
 		fmt.Println(err.Error())
-		out.Dst = ""
-		out.Msg = err.Error()
-		out.Status = 500
-
-		return err
+		return &pb.DownloadResponse{Dst: dst, Status: 500, Msg: err.Error()}, err
 
 	}
-	out.Dst = dst
-	out.Status = 200
-	out.Msg = "success"
-	return nil
+	return &pb.DownloadResponse{Dst: dst, Status: 200}, nil
 
 }
 
 func StartServer(server ServerInterface) {
-	grpcServer := grpc.NewServer()
-	service := micro.NewService(
-		micro.Server(grpcServer),
-		micro.Name("downloader.service"),
-		micro.Address("0.0.0.0:9527"),
-		micro.RegisterTTL(time.Second*30),
-		micro.RegisterInterval(time.Second*10),
-		micro.Registry(registry.NewRegistry(registry.Addrs("127.0.0.1:2379"))),
-	)
-
-	// optionally setup command line usage
-	service.Init()
-
-	// Register Handlers
-	pb.RegisterDownloaderHandler(service.Server(), server)
-
-	// Run server
-	if err := service.Run(); err != nil {
-		log.Fatal(err)
+	lis, err := net.Listen("tcp", ":9527")
+	if err != nil {
+		fmt.Printf("failed to listen: %v", err)
+		return
+	}
+	s := grpc.NewServer()                  // 创建gRPC服务器
+	pb.RegisterDownloaderServer(s, server) // 在gRPC服务端注册服务
+	err = s.Serve(lis)
+	if err != nil {
+		panic(err)
 	}
 }
